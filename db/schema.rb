50fa4c00c7aec7307a41cd9ef648fc34aaa6116c
David Marchante <davidmarchan@gmail.com>
2016-03-27 18:57:49 +0200

    Created Tag model and its associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160327122341) do
+ActiveRecord::Schema.define(version: 20160327164724) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts_tags", id: false, force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "tag_id",  null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
