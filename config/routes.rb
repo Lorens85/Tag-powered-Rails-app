618ea9a8fdfd457ee6bf63f858a88c58b9196f0a
David Marchante <davidmarchan@gmail.com>
2016-03-28 14:34:50 +0200

    Added a Tag cloud

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
   root 'posts#index'
-  get 'tag/:name' => 'tags#filter', as: :tag_filter
+  get 'tag/:name'  => 'tags#filter', as: :tag_filter
+  get 'api/cloud' => 'tags#cloud', defaults: { format: 'json' }
 end
